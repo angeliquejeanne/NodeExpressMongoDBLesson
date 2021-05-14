b77bbcd39f57f12e651fb35e6658ed39c93e2d1f
anggiie <anggiie02>
2021-05-14 18:05:25 +0200

    add end part four

--- routes/stuff.js
+++ routes/stuff.js
@@ -3,9 +3,10 @@ const router = express.Router();
 
 const stuffCtrl = require('../controllers/stuff');
 const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
-router.post('/', auth, stuffCtrl.createThing);
-router.put('/:id', auth, stuffCtrl.modifyThing);
+router.post('/', auth, multer, stuffCtrl.createThing);
+router.put('/:id', auth, multer, stuffCtrl.modifyThing);
 router.delete('/:id', auth, stuffCtrl.deleteThing);
 router.get('/:id', auth, stuffCtrl.getOneThing);
 router.get('/', auth, stuffCtrl.getAllThings);
